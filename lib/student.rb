216769831c544dff940421e81f8114873676bc8c
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-15 03:08:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,6 +85,11 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(number)
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      where grade = 10
+    SQL
+    DB[:conn].execute(sql)
   end
 end
